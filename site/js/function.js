08fcfb84f90582f2ef29f0c9647bbee356b5d2c0
itggot-Anna-Nilsson <anna.nilsson@itggot.se>
2017-05-30 15:42:12 +0200

    fixed half of the menu

--- site/js/function.js
+++ site/js/function.js
@@ -1,2 +1,24 @@
 
+function slide(){
+  var darkbox = document.querySelector("#darkbox");
+  if(!darkbox.classList.contains('opacity50') && !darkbox.classList.contains('opacity0')){
+    darkbox.classList.toggle('opacity0');
+    darkbox.classList.remove('darkbox');
+  }
+  else {
+    darkbox.classList.toggle('opacity0');
+    darkbox.classList.toggle('opacity50');
+    darkbox.classList.remove('darkbox');
+  }
+
+  var element = document.querySelector("#navi");
+  console.log("slide");
+  if(!element.classList.contains('menuhidden') && !element.classList.contains('menuvisible')){
+    element.classList.toggle('menuvisible');
+  }
+  else {
+    element.classList.toggle('menuvisible');
+    element.classList.toggle('menuhidden');
+  }
+}
 //alert("hello")
